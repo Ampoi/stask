8e29484f4d9e20c32099cd18fb01450e61e52361
Ampoi <shibata.kuronosuke.offcial@gmail.com>
2022-08-31 13:00:14 +0900

    PWA対応

--- src/main.js
+++ src/main.js
@@ -4,8 +4,11 @@ import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 import "./index.css"
 import router from "./router"
+import { registerSW } from "virtual:pwa-register"
 
 loadFonts()
+
+registerSW()
 
 createApp(App)
   .use(vuetify)
--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,21 @@ import vue from '@vitejs/plugin-vue'
 // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
 import vuetify from 'vite-plugin-vuetify'
 
+import { VitePWA } from 'vite-plugin-pwa'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
 		vue(),
 		vuetify({ autoImport: true }),
+		VitePWA({
+      registerType: 'autoUpdate', 
+      manifest: {
+        // content of manifest
+      },
+      workbox: {
+        // workbox options for generateSW
+      }
+    })
 	]
 })
