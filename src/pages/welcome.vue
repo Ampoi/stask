d8923feceebb1b1e5fe8a88c3930e54371cbf826
Ampoi <shibata.kuronosuke.offcial@gmail.com>
2023-01-28 21:47:19 +0900

    welcomeが表示されない問題の修正

--- src/pages/welcome.vue
+++ src/pages/welcome.vue
@@ -64,6 +64,7 @@
           :card="card"
           :onlydone="true"
           :beShowed="true"
+          :subjects="subjects"
         />
       </div>
       
@@ -126,7 +127,14 @@ export default {
       done: true,     
       subject: 1
     },
-    headerColor: true
+    headerColor: true,
+    subjects: [
+      {index:0, title: "国語 (古文/現代文)", color:"#F44335"},
+      {index:1, title: "数学 (算数)", color:"#2196F3"},
+      {index:2, title: "理科 (物理/地学/生物/化学)", color:"#4BAF51"},
+      {index:3, title: "社会 (公民/地理/歴史)", color:"#FFC105"},
+      {index:4, title: "英語 (外国語)", color: "#E040FB"}
+    ]
   }},
   async mounted(){
     const updateData = await fetch("https://api.github.com/repos/ampoi/stask/issues?labels=new-release")
