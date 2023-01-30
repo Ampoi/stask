45a6f5446910e1104c3db2757eca85880b8a0b83
Ampoi <shibata.kuronosuke.offcial@gmail.com>
2023-01-30 19:39:08 +0900

    404の追加

---
+++ src/pages/notFound.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="w-screen h-screen grid place-content-center">
+    <div class="flex flex-col items-center">
+      <h1 class="text-9xl font-bold">404</h1>
+      <p>お探しのページは見つかりませんでした。</p>
+      <a href="/" class="py-2 px-4 bg-orange-400 rounded-full text-white font-bold mt-4">Staskホームページへ戻る</a>
+    </div>
+  </div>
+</template>
