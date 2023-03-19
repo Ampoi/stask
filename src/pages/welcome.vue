ad7c9636eb145577f371210461bee4efcbd1b78a
Ampoi <shibata.kuronosuke.offcial@gmail.com>
2023-03-19 11:36:40 +0900

    ヘッダーのデザインを変更

--- src/pages/welcome.vue
+++ src/pages/welcome.vue
@@ -1,14 +1,16 @@
 <template>
   <v-app class="bg-slate-100">
-    <v-app-bar
-      flat class="bg-transparent px-8 py-3"
-    >
-      <template v-slot:append>
+    <v-app-bar flat class="bg-transparent px-8 py-3">
+      <div class="w-[800px] mx-auto flex flex-row items-center gap-2 justify-between">
+        <a class="flex-row flex gap-2 items-center">
+          <img src="../assets/logo.svg" class="w-6">
+          <p class="text-2xl font-bold">Stask</p>
+        </a>
         <a
           class="bg-gradient-to-bl from-orange-300 to-red-400 font-bold text-white py-2.5 px-6 rounded-full"
           href="/login"
         >Get Started</a>
-      </template>
+      </div>
     </v-app-bar>
     <v-main>
       <main class="flex flex-col">
