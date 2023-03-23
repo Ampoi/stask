de280a736a8c37d9adb360037beb68f12e9f7613
S.kuronosuke <tange.toshihiro@outlook.jp>
2023-03-23 11:56:18 +0900

    設定の項目のコンポーネント化

--- src/components/settings.vue
+++ src/components/settings.vue
@@ -18,42 +18,48 @@
     </div>
 
     <h1 class="text-3xl font-bold mb-2">設定</h1>
-    <h2 class="text-xl">教科の設定</h2>
-    <p>課題をまとめる教科の色や名前の設定を変更します。</p>
-    <div class="p-4 my-2 bg-orange-100 border-orange-200 border-2 border-solid rounded-md flex flex-row items-center gap-4">
-      <v-icon>mdi-alert-outline</v-icon>
-      <p class="text-sm">設定項目の数を減らしたり順番を変えたりすると課題の表示に影響が出る場合があります。</p>
-    </div>
-    <div class="bg-gray-100 p-4 rounded-md">
-      <div class="pb-4 flex flex-col gap-4 items-center max-h-40 overflow-y-auto">
-        <div
-          class="flex flex-row items-center gap-2"
-          v-for="(subject, subIndex) in settings.subjects"
-          :key="subIndex"
-        >
-          <button
-            class="rounded-full h-6 w-6"
-            :style="`background-color: ${subject.color}6F;`"
-            @click="$emit('getSubjectColor', subIndex)"
-          />
-          <input type="text" v-model="subject.title">
-          <button
-            class="rounded-full h-6 w-6 duration-300 hover:bg-white/70 text-[14px] text-red-400/60 grid place-content-center"
-            @click="$emit('deleteSubject', subIndex)"
-          ><v-icon>mdi-trash-can</v-icon></button>
+    <SettingList>
+      <template v-slot:title>教科の設定</template>
+      <template v-slot:description>課題をまとめる教科の色や名前の設定を変更します。</template>
+      <template v-slot:warning>
+        <div class="p-4 my-2 bg-orange-100 border-orange-200 border-2 border-solid rounded-md flex flex-row items-center gap-4">
+          <v-icon>mdi-alert-outline</v-icon>
+          <p class="text-sm">設定項目の数を減らしたり順番を変えたりすると課題の表示に影響が出る場合があります。</p>
         </div>
-      </div>
-      <button
-        class="text-gray-600 bg-white rounded-md p-2 w-full"
-        @click="$emit('addSubject')"
-      >
-        <v-icon>mdi-plus</v-icon>
-      </button>
-    </div>
+      </template>
+      <template v-slot:main>
+        <div class="pb-4 flex flex-col gap-4 items-center max-h-40 overflow-y-auto">
+          <div
+            class="flex flex-row items-center gap-2"
+            v-for="(subject, subIndex) in settings.subjects"
+            :key="subIndex"
+          >
+            <button
+              class="rounded-full h-6 w-6"
+              :style="`background-color: ${subject.color}6F;`"
+              @click="$emit('getSubjectColor', subIndex)"
+            />
+            <input type="text" v-model="subject.title">
+            <button
+              class="rounded-full h-6 w-6 duration-300 hover:bg-white/70 text-[14px] text-red-400/60 grid place-content-center"
+              @click="$emit('deleteSubject', subIndex)"
+            ><v-icon>mdi-trash-can</v-icon></button>
+          </div>
+        </div>
+        <button
+          class="text-gray-600 bg-white rounded-md p-2 w-full"
+          @click="$emit('addSubject')"
+        >
+          <v-icon>mdi-plus</v-icon>
+        </button>
+      </template>
+    </SettingList>
   </div>
 </template>
 <script setup lang="ts">
 import { ref as vueData, watch } from "vue"
+
+import SettingList from "./settings/settingList.vue"
 
 import type { Settings } from "../types/settings";
 
---
+++ src/components/settings/settingList.vue
@@ -0,0 +1,8 @@
+<template>
+	<h2 class="text-xl"><slot name="title"/></h2>
+	<p><slot name="description"/></p>
+	<slot name="warning"/>
+	<div class="bg-gray-100 p-4 rounded-md">
+		<slot name="main"/>
+	</div>
+</template>
