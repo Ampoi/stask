3274c929294fb16211872d0f3671dc03b5855cc1
Ampoi <tange.toshihiro@outlook.jp>
2023-04-29 18:24:58 +0900

    お問い合わせフォームの大画面向けUIの作成

--- src/pages/contact.vue
+++ src/pages/contact.vue
@@ -5,19 +5,22 @@
       <h1 class="text-5xl font-black">お問い合わせ</h1>
       <p class="text-sm pt-2">Staskで発生したバグ・改善をお問い合わせください</p>
       
-      <div class="px-4 py-8 rounded-md bg-white max-w-xl mt-8 mx-auto flex flex-col">
-        <h2 class="text-3xl font-bold">お問い合わせフォーム</h2>
-        <p>バグの報告や改善案を送りたい場合は<br>以下のお問い合わせフォームから送信してください</p>
-        <iframe :src="googleFormURL" height="400" frameborder="0" marginheight="0" marginwidth="0">読み込んでいます…</iframe>
-      </div>
-      <p class="py-4">もしくは</p>
-      <div class="px-4 py-8 rounded-md bg-white max-w-xl mx-auto flex flex-col">
-        <h2 class="text-3xl font-bold">Discordサーバー</h2>
-        <p>継続的にバグの報告・新機能の案を送りたい場合は<br>Discordサーバーでお聞かせください</p>
-        <a
-          :href="discordInviteURL"
-          class="mt-2 underline"
-        >Discordサーバー</a>
+      <div class="flex flex-col mt-8 gap-4 max-w-xl mx-auto
+                  desktop:flex-row desktop:items-center desktop:max-w-[calc(864px+2rem)]">
+        <div class="px-4 py-8 rounded-md bg-white flex flex-col desktop:basis-[400px]">
+          <h2 class="text-3xl font-bold">お問い合わせフォーム</h2>
+          <p>バグの報告や改善案を送りたい場合は<br>以下のお問い合わせフォームから送信してください</p>
+          <iframe :src="googleFormURL" height="400" frameborder="0" marginheight="0" marginwidth="0">読み込んでいます…</iframe>
+        </div>
+        <p class="text-gray-500">もしくは</p>
+        <div class="px-4 py-8 rounded-md bg-white flex flex-col desktop:basis-[400px]">
+          <h2 class="text-3xl font-bold">Discordサーバー</h2>
+          <p>継続的にバグの報告・新機能の案を送りたい場合は<br>Discordサーバーでお聞かせください</p>
+          <a
+            :href="discordInviteURL"
+            class="mt-2 underline"
+          >Discordサーバー</a>
+        </div>
       </div>
     </v-main>
     <v-footer>  
