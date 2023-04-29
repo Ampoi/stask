afed2e2aa322e29d90485358cc45a295173d428f
Ampoi <tange.toshihiro@outlook.jp>
2023-04-29 16:57:46 +0900

    footerのデザインの変更

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,9 +1,8 @@
 <template>
   <v-app-bar flat class="bg-transparent px-8 py-3">
     <div class="w-[800px] mx-auto flex flex-row items-center gap-2 justify-between">
-      <a class="flex-row flex gap-2 items-center" href="/welcome">
-        <img src="../assets/logo.svg" class="w-6">
-        <p class="text-2xl font-bold">Stask</p>
+      <a href="/welcome">
+        <Logo/>
       </a>
       <a
         class="bg-gradient-to-bl from-orange-300 to-red-400 font-bold text-white py-2.5 px-6 rounded-full"
@@ -12,3 +11,6 @@
     </div>
   </v-app-bar>
 </template>
+<script setup lang="ts">
+import Logo from './logo.vue';
+</script>
