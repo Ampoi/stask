3bfc25409fd1fea637ddce2109b11c0355839460
Ampoi <tange.toshihiro@outlook.jp>
2023-07-10 11:56:26 +0900

    ページのUI・教科選択・taskcardの削除UIの作成

--- src/components/App/Group/TaskCard.vue
+++ src/components/App/Group/TaskCard.vue
@@ -28,44 +28,76 @@
         <TransitionRoot
             :show="showCardMenu.value"
             enter-from="h-0"
-            enter="duration-300 ease-out"
-            enter-to="h-80"
-            leave-from="h-80"
-            leave="duration-300 ease-in"
+            enter="duration-300 ease-out overflow-hidden"
+            enter-to="h-[280px]"
+            leave-from="h-[280px]"
+            leave="duration-300 ease-in overflow-hidden"
             leave-to="h-0"
-            class="overflow-hidden flex flex-col gap-2"
+            class="flex flex-col gap-2"
             >
-            <div class="w-full bg-gray-100 border-gray-200 border-[1px] h-56 rounded-lg p-4 flex flex-col gap-2 relative overflow-y-auto">
-                <PageUnitOptions v-model:page-unit="cardUnit"/>
-                <div class="flex flex-row gap-2 items-center text-xl py-3 px-4 border-white border-[1px] rounded-lg">
-                    <span>{{ cardUnit.symbol(30) }}</span>
-                    <ProgressBar :percent="80" sub-color="white"/>
-                    <span>{{ cardUnit.symbol(30) }}</span>
-                </div>
-                <div class="flex flex-row gap-2 items-center text-xl py-3 px-4 border-white border-[1px] rounded-lg">
-                    <span>{{ cardUnit.symbol(30) }}</span>
-                    <ProgressBar :percent="80" sub-color="white"/>
-                    <span>{{ cardUnit.symbol(30) }}</span>
-                </div>
-                <div class="flex flex-row gap-2 items-center text-xl py-3 px-4 border-white border-[1px] rounded-lg">
-                    <span>{{ cardUnit.symbol(30) }}</span>
-                    <ProgressBar :percent="80" sub-color="white"/>
-                    <span>{{ cardUnit.symbol(30) }}</span>
+            <div class="w-full bg-gray-100 border-gray-200 border-[1px] h-56 rounded-lg p-4 flex flex-col gap-2 overflow-y-auto">
+                <div class="grow overflow-scroll flex flex-col gap-2">
+                    <PageUnitOptions v-model:page-unit="cardUnit"/>
+                    <Page :symbol-function="cardUnit.symbol"/>
                 </div>
                 <button
-                    class="rounded-lg p-2 bg-white w-full sticky left-0 bottom-0">
+                    class="rounded-lg p-2 bg-white w-full">
                     <i class="bi bi-file-earmark-plus"></i>
+                </button>
+            </div>
+            <div class="flex flex-row items-stretch gap-2 h-10">
+                <Listbox
+                    v-model="cardSubject"
+                    v-slot="{ open }"
+                    as="div"
+                    class="grow">
+                    <ListboxButton
+                        class="rounded-lg p-2 border-[1px] border-gray-100 w-full"
+                        :class="{ 'rounded-b-none': open }">
+                        教科：{{ cardSubject.name }}
+                    </ListboxButton>
+                    <TransitionRoot
+                        enter-from="opacity-0 ease-out -translate-y-10"
+                        enter="duration-300"
+                        enter-to="opacity-100 translate-y-0"
+                        leave-from="opacity-100 translate-y-0"
+                        leave-to="opacity-0 ease-in -translate-y-10"
+                        leave="duration-300"
+                        class="relative w-full">
+                        <ListboxOptions
+                            class="p-2 w-full bg-white rounded-b-lg border-[1px] border-t-0 border-gray-100 flex flex-col absolute"
+                            ref="unitsOptions">
+                            <ListboxOption
+                                v-for="(subject, index) in subjects"
+                                :key="index"
+                                :value="subject"
+                                class="p-1 text-center">
+                                {{ subject.name }}
+                            </ListboxOption>
+                        </ListboxOptions>
+                    </TransitionRoot>
+                </Listbox>
+                <button class="rounded-lg border-[1px] border-red-400 text-red-400 text-lg w-10 grid place-content-center">
+                    <i class="bi bi-trash3"></i>
                 </button>
             </div>
         </TransitionRoot>
     </div>
+    <!--
+    <Dialog
+        :open="showEditCardModal.value"
+        class="fixed bottom-0 w-screen h-screen bg-black/20 p-0"
+        @click="showEditCardModal.turn()">
+        <div class="w-full h-5/6 fixed bottom-0 rounded-t-3xl mt-auto bg-white p-6">aaa</div>
+    </Dialog>-->
 </template>
 <script setup lang="ts">
 import { ref } from "vue"
-import { TransitionRoot } from "@headlessui/vue"
+import { Listbox, ListboxButton, ListboxOption, ListboxOptions, TransitionRoot } from "@headlessui/vue"
 
 import PageUnitOptions from "./pageUnitOptions.vue";
 import ProgressBar from "./progressBar.vue";
+import Page from "./page.vue"
 
 class Switch {
     switch: boolean
@@ -83,11 +115,23 @@ class Switch {
     }
 }
 
+
 const showCardMenu = ref(new Switch(false))
+//const showEditPageModal = ref(new Switch(true))
 const cardUnit = ref({ name: "ページ", symbol: (page: number): string => {return `p.${page}`} })
 
 const card = {
     done: true,
     name: "数学A"
 }
+
+
+const subjects = [
+    { name: "国語", color: "#F44335" },
+    { name: "数学", color: "#2196F3" },
+    { name: "理科", color: "#4BAF51" },
+    { name: "社会", color: "#FFC105" },
+    { name: "英語", color: "#E040FB" }
+]
+const cardSubject = subjects[0]
 </script>
---
+++ src/components/App/Group/page.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="flex flex-row gap-2 items-center text-xl py-3 px-4 border-white border-[1px] rounded-lg">
+		<span>{{ props.symbolFunction(30) }}</span>
+		<ProgressBar :percent="80" sub-color="white"/>
+		<span>{{ props.symbolFunction(30) }}</span>
+	</div>
+</template>
+<script setup lang="ts">
+import ProgressBar from "./progressBar.vue";
+
+const props = defineProps<{
+	symbolFunction: (page: number) => string
+}>()
+</script>
