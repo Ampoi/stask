ec2cd7e3f0b900a4285146ce331567cb61f4c250
Ampoi <tange.toshihiro@outlook.jp>
2023-07-10 16:48:50 +0900

    グループの画面の時に課題を追加するボタンを表示するようにした

--- src/components/App/NavBar.vue
+++ src/components/App/NavBar.vue
@@ -1,15 +1,29 @@
 <template>
     <div class="bottom-12 w-[calc(100%-48px)] h-16 flex flex-row gap-4">
         <Menu as="div" class="relative">
-            <MenuButton class="w-16 h-16 bg-orange-300 rounded-full">
-                <i class="bi bi-list text-3xl text-white"/>
+            <MenuButton class="w-16 h-16 bg-orange-300 text-white rounded-full grid place-content-center">
+                <i class="bi bi-list text-3xl"/>
             </MenuButton>
             <NavBarMenu/>
         </Menu>
+        <button
+            v-if="isGroupPage"
+            class="grow rounded-full bg-orange-300 text-white grid place-content-center">
+            <div class="flex flex-row gap-2 items-center">
+                <i class="bi bi-journal-plus text-3xl"/>
+                <span class="text-xl">課題を追加する</span>
+            </div>
+        </button>
     </div>
 </template>
 <script setup lang="ts">
 import { Menu, MenuButton } from "@headlessui/vue"
 
 import NavBarMenu from "./NavBar/Menu.vue"
+
+const props = defineProps<{
+    nowPage: string
+}>()
+
+const isGroupPage = props.nowPage == "group"
 </script>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,11 +1,19 @@
 <template>
     <div class="bg-gray-100 w-screen h-screen px-6 py-12 box-border relative">
-        <component :is="Group"/>
-        <NavBar class="absolute"/>
+        <component :is="pages[nowPage]"/>
+        <NavBar
+            class="absolute"
+            :now-page="nowPage"/>
     </div>
 </template>
 <script setup lang="ts">
 import NavBar from "../components/App/NavBar.vue"
 
 import Group from "../components/App/Group.vue"
+
+const pages = {
+    group: Group
+}
+
+const nowPage: keyof typeof pages = "group"
 </script>
