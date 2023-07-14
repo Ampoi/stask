3832c20bde4bd540e4ab35c5efcecc73c7dcdf0c
Ampoi <tange.toshihiro@outlook.jp>
2023-07-14 15:36:01 +0900

    メンバーの権限の管理をできるようにした

--- src/components/App/Group/groupSettingsModal/member.vue
+++ src/components/App/Group/groupSettingsModal/member.vue
@@ -7,6 +7,8 @@
                 background: `url(${member.icon})`,
             }"/>
         <p class="grow p-2">{{ member.name }}</p>
+        <MemberRoleOption
+            v-model:member-role="member.role"/>
         <button
             class="w-8 h-8 rounded-full grid place-content-center text-red-400 bg-red-200/80">
             <i class="bi bi-trash3 text-md"/>
@@ -14,9 +16,11 @@
     </div>
 </template>
 <script setup lang="ts">
+import MemberRoleOption from "./memberRoleOption.vue";
+
 import { Member } from "../../../../models/groupSettings"
 
 const props = defineProps<{
     member: Member
-}>()
+}>()//TODO:Propsだけでv-modelみたいなことするのをやめたい
 </script>
