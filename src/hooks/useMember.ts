e61b09f2dec63083bbe31351bf5e130b4c39ab63
Ampoi <tange.toshihiro@outlook.jp>
2023-07-17 21:31:45 +0900

    メンバーかの確認のデータの取得範囲を狭めた

--- src/hooks/useMember.ts
+++ src/hooks/useMember.ts
@@ -2,16 +2,15 @@ import { createMembersRepository } from "../infra/membersRepository";
 import useAuth from "./useAuth";
 
 export default async (groupID: string) => {
-    const membersRepository = createMembersRepository(groupID)
+    const { isLogin, getUserData } = await useAuth()
+    if( !isLogin ){ return false }
+    const { uid } = await getUserData()
+
+    const membersRepository = createMembersRepository(groupID, uid)
 
     const isMember = await (async () => {
-        const { isLogin, getUserData } = await useAuth()
-        if( !isLogin ){ return false }
-
-        const { uid } = await getUserData()
-
-        const members = await membersRepository.get() ?? {}
-        return !!members[uid]
+        const myMemberData = await membersRepository.get()
+        return !!myMemberData
     })()
 
     return { isMember }
