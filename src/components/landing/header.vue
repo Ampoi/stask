5a49fb40dcaccf360f09735d4070c6b1dbd01acb
Ampoi <tange.toshihiro@outlook.jp>
2023-07-19 21:42:37 +0900

    ランディングページのデザインを移行

---
+++ src/components/landing/header.vue
@@ -0,0 +1,17 @@
+<template>
+    <v-app-bar flat class="bg-transparent px-8 py-3">
+        <div class="w-[800px] mx-auto px-4 flex flex-row items-center gap-2 justify-between">
+            <a href="/welcome">
+                <Logo />
+            </a>
+            <a
+                class="bg-gradient-to-bl from-orange-300 to-red-400 font-bold text-white py-2.5 px-6 rounded-full"
+                href="/login">
+                Get Started
+            </a>
+        </div>
+    </v-app-bar>
+</template>
+<script setup lang="ts">
+import Logo from './logo.vue';
+</script>
