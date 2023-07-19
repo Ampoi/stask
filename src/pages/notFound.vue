4a76d2136ffdf2347c2304db943d356b42c135fe
Ampoi <tange.toshihiro@outlook.jp>
2023-07-19 21:09:58 +0900

    404ページの追加

---
+++ src/pages/notFound.vue
@@ -0,0 +1,21 @@
+<template>
+    <main class="grid place-content-center h-screen">
+        <div class="max-w-xl text-center mx-4 flex flex-col items-center gap-4">
+            <div>
+                <h1 class="text-8xl font-semibold">404</h1>
+                <p>お探しのページ「{{ route.params.catchAll }}」は見つかりませんでした</p>
+            </div>
+            <RouterLink
+                to="/"
+                class="px-4 py-3 border-gray-100 border-2 rounded-md flex flex-row gap-2 items-baseline justify-center">
+                <i class="bi bi-door-closed"/>
+                <p>紹介ページにいく</p>
+            </RouterLink>
+        </div>
+    </main>
+</template>
+<script setup lang="ts">
+import { useRoute } from 'vue-router';
+
+const route = useRoute()
+</script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -4,13 +4,15 @@ import Landing from "../pages/Landing.vue"
 import Home from "../pages/home.vue"
 import Group from "../pages/group.vue"
 import Invite from "../pages/Invite.vue"
+import NotFound from "../pages/notFound.vue"
 
 export const router = createRouter({
     routes: [
         { path: "/", name: "Landing", component: Landing },
         { path: "/:home(app|group)", name: "Home", component: Home },
         { path: "/group/:groupID", name: "Group", component: Group },
-        { path: "/invite/:groupID/:inviteID", name: "Invite", component: Invite }
+        { path: "/invite/:groupID/:inviteID", name: "Invite", component: Invite },
+        { path: "/:catchAll(.*)", name: "NotFound", component: NotFound }
     ],
     history: createWebHistory()
 })
