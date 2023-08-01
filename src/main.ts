ac80b723a0b13c6db6a69e9c39a3ede07cd267ec
Ampoi <tange.toshihiro@outlook.jp>
2023-08-01 18:42:24 +0900

    PWAにできるようにした

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,10 @@ import { router } from './router'
 import "bootstrap-icons/font/bootstrap-icons.css"
 import "./style.css"
 import { loadFonts } from "./plugins/webfontloader"
+import { registerSW } from 'virtual:pwa-register'
 
 loadFonts()
+registerSW();
 
 createApp(App)
     .use(router)
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from "vite-plugin-pwa"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        lang: 'ja',
+        name: 'Stask',
+        short_name: 'Stask',
+        background_color: '#F3F4F6',
+        theme_color: '#F3F4F6',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'touch_icon.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+        ]
+      }
+    })
+  ],
 })
