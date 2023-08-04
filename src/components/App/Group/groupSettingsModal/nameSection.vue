152cb83f210be711097d369faba9db3c15277f93
Ampoi <tange.toshihiro@outlook.jp>
2023-08-04 22:16:50 +0900

    v-modelをcomputedで制御するのを省略

--- src/components/App/Group/groupSettingsModal/nameSection.vue
+++ src/components/App/Group/groupSettingsModal/nameSection.vue
@@ -5,21 +5,17 @@
             <input
                 type="text"
                 class="p-2 rounded-md border-white border-[1px] bg-transparent grow"
-                v-model="editableName"
+                :value="props.name"
+                @input="
+                    //@ts-ignore
+                    (event) => emit('update:name', event.target.value)"
                 placeholder="学校 / 3-Aのグループ / etc...">
         </div>
     </ModalSection>
 </template>
 <script setup lang="ts">
-import { computed } from "vue";
-
 import ModalSection from "../../modal/section.vue"
 
 const props = defineProps<{ name: string }>()
 const emit = defineEmits<{ (e: "update:name", newName: string): void }>()
-
-const editableName = computed({
-    get(){ return props.name },
-    set(newName: string){ emit("update:name", newName) }
-})
 </script>
--- src/components/App/Group/groupSettingsModal/noteSection.vue
+++ src/components/App/Group/groupSettingsModal/noteSection.vue
@@ -4,21 +4,17 @@
             <p class="text-lg">グループの注意書き</p>
             <textarea
                 class="p-2 rounded-md border-white border-[1px] bg-transparent grow resize-none"
-                v-model="editableNote"
+                :value="props.note"
+                @update="
+                    //@ts-ignore
+                    (event) => emit('update:note', event.target.value)"
                 placeholder="共有されているタスクで編集できるものは編集して大丈夫です"/>
         </div>
     </ModalSection>
 </template>
 <script setup lang="ts">
-import { computed } from "vue";
-
 import ModalSection from "../../modal/section.vue"
 
 const props = defineProps<{ note: string }>()
 const emit = defineEmits<{ (e: "update:note", newNote: string): void }>()
-
-const editableNote = computed({
-    get(){ return props.note },
-    set(newNote: string){ emit("update:note", newNote) }
-})
 </script>
