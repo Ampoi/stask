7fbbcd3fc464e1228f51d54b81b9ca7bcb6c5016
Ampoi <tange.toshihiro@outlook.jp>
2023-08-05 13:39:36 +0900

    達成度をuseTaskで補完するようにした

--- src/hooks/useTasks.ts
+++ src/hooks/useTasks.ts
@@ -9,13 +9,26 @@ export default async (groupID: string) => {
     const tasksData: Task[] = await (async () => {
         const newPartialTasks = await taskRepository.get() ?? []
         const { groupSettings } = await useGroupSettings(groupID)
+
         const newTasks: Task[] = newPartialTasks.map((newPartialTask) => {
             const newTask = { ...Task.create(groupSettings.value.subjects), ...newPartialTask }
+
             const newScopes = newTask.scopes.map((newTaskScope) => {
-                return { ...Scope.create(), ...newTaskScope }
+                const newScope = { ...Scope.create(), ...newTaskScope }
+                const members = groupSettings.value.members
+                let newScopeNows: { [key: string]: number } = {}
+                Object.entries(members).forEach(member => {
+                    const [ uid ] = member
+                    const oldScopeNow: number | undefined = newScope.now[uid]
+                    newScopeNows[uid] = oldScopeNow ?? newScope.first
+                });
+
+                return { ...newScope, ...{now: newScopeNows} }
             })
+            console.log({ ...newTask, ...{ scopes: newScopes } })
             return { ...newTask, ...{ scopes: newScopes } }
         })
+
         return newTasks ?? []
     })()
     
