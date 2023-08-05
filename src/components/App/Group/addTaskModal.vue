f7b3683a6af1caa158b866b2b3a44dd2ccc4b9fc
Ampoi <tange.toshihiro@outlook.jp>
2023-08-05 16:35:25 +0900

    テキストから課題を追加する画面の追加

--- src/components/App/Group/addTaskModal.vue
+++ src/components/App/Group/addTaskModal.vue
@@ -4,6 +4,20 @@
         @update:open="(newOpen) => emit('update:open', newOpen)">
         <div class="flex flex-col gap-4 items-center h-full">
             <h1 class="text-3xl font-semibold">課題を追加する</h1>
+            <ModalSection class="flex flex-row gap-4 w-full">
+                <button
+                    class="p-2 bg-white rounded-md text-center transition duration-150 basis-1/2"
+                    :class="(editWith == 'EditWithText') ? 'text-black' : 'text-black/40'"
+                    @click="editWith = 'EditWithText'">
+                    テキスト
+                </button>
+                <button
+                    class="p-2 bg-white rounded-md text-center transition duration-150 basis-1/2"
+                    :class="(editWith == 'EditWithManual') ? 'text-black' : 'text-black/40'"
+                    @click="editWith = 'EditWithManual'">
+                    手動
+                </button>
+            </ModalSection>
             <component
                 :is="edits[editWith]"
                 :groupID="groupID"
@@ -13,6 +27,7 @@
 </template>
 <script setup lang="ts">
 import Modal from "../modal.vue"
+import ModalSection from "../modal/section.vue"
 
 import { Task } from "../../../models/task";
 
--- src/components/App/Group/addTaskModal/editWithText.vue
+++ src/components/App/Group/addTaskModal/editWithText.vue
@@ -1,1 +1,47 @@
-<template></template>
+<template>
+    <ModalSection class="mx-4 grow w-full flex flex-col gap-4 overflow-y-auto">
+        <div class="flex flex-col gap-2 items-start p-2">
+            <p class="text-lg">課題に関する情報</p>
+            <p class="text-sm text-black/40">「漢字1~15、8/31まで」などの文章からタスクを生成できます</p>
+            <input
+                type="text"
+                class="p-2 rounded-md border-white border-[1px] bg-transparent grow w-full"
+                v-model="newTaskText"
+                placeholder="漢字 / 数学問題集 / etc...">
+        </div>
+    </ModalSection>
+    <button
+        class="basis-16 min-h-[64px] w-full rounded-full bg-orange-300 text-white grid place-content-center"
+        @click="addTask()">
+        <div class="flex flex-row gap-2 items-center">
+            <i class="bi bi-journal-plus text-3xl"/>
+            <span class="text-xl">課題を追加する</span>
+        </div>
+    </button>
+</template>
+<script setup lang="ts">
+import { ref } from "vue";
+import { Task } from "../../../../models/task";
+import ModalSection from "../../modal/section.vue"
+import useGroupSettings from "../../../../hooks/useGroupSettings";
+
+const props = defineProps<{
+    groupID: string
+}>()
+
+const emit = defineEmits<{
+    (e: "addTask", newTask: Task): void
+}>()
+
+const newTaskText = ref("")
+
+const { groupSettings } = await useGroupSettings(props.groupID)
+const subjects = groupSettings.value.subjects
+
+const newTask = ref(Task.create(subjects))
+
+function addTask(){
+    emit("addTask", newTask.value)
+    newTask.value = Task.create(subjects)
+}
+</script>
