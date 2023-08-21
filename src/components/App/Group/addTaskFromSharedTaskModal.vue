61424122295b6dec9e7e1a1ed0f5771d89f62665
Ampoi <tange.toshihiro@outlook.jp>
2023-08-21 14:48:44 +0900

    共有されている課題を全員に追加するボタンの作成

--- src/components/App/Group/addTaskFromSharedTaskModal.vue
+++ src/components/App/Group/addTaskFromSharedTaskModal.vue
@@ -1,102 +1,122 @@
 <template>
-    <Modal
-        :open="props.open"
-        @update:open="(newOpen) => emit('update:open', newOpen)">
-        <div class="flex flex-col gap-4 items-center h-full">
-            <h1 class="text-3xl font-semibold">追加する課題を選択</h1>
-            <div class="grow flex flex-col w-full">
-                <button
-                    class="mb-2 border-gray-200 border-2 text-gray-400 p-4 rounded-md flex flex-row gap-2 justify-center items-center"
-                    @click="createTask">
-                    <i class="bi bi-journal-plus text-2xl"/>
-                    <p>課題を新しく作成する</p>
-                </button>
-                <div
-                    v-for="task in tasks"
-                    :key="task.id">
-                    <SharedTaskButton 
-                        v-if="!task.deleted && !task.workon.includes(uid)"   
-                        :task="task"
-                        :groupID="groupID"
-                        :isInQue="que.includes(task.id)"
-                        @selected="addTaskToQue(task.id)"
-                        class="mb-2"/>
-                </div>
-            </div>
-            <button
-                class="basis-16 min-h-[64px] w-full rounded-full bg-orange-300 text-white grid place-content-center"
-                @click="addTask">
-                <div class="flex flex-row gap-2 items-center">
-                    <i class="bi bi-journal-plus text-2xl"/>
-                    <span class="text-xl">選択した課題を追加する</span>
-                </div>
-            </button>
+  <Modal
+    :open="props.open"
+    @update:open="(newOpen) => emit('update:open', newOpen)"
+  >
+    <div class="flex flex-col gap-4 items-center h-full">
+      <h1 class="text-3xl font-semibold">追加する課題を選択</h1>
+      <div class="grow flex flex-col w-full">
+        <button
+          class="mb-2 border-gray-200 border-2 text-gray-400 p-4 rounded-md flex flex-row gap-2 justify-center items-center"
+          @click="createTask"
+        >
+          <i class="bi bi-journal-plus text-2xl" />
+          <p>課題を新しく作成する</p>
+        </button>
+        <div v-for="task in tasks" :key="task.id">
+          <SharedTaskButton
+            v-if="!task.deleted && !task.workon.includes(uid)"
+            :task="task"
+            :groupID="groupID"
+            :isInAddQue="addQue.includes(task.id)"
+            @selected="addTaskToQue(task.id)"
+            @promoteThisTask="promoteTask(task.id)"
+            class="mb-2"
+          />
         </div>
-    </Modal>
-    <CreateTaskModal
-        v-model:open="showCreateTaskModal"
-        :groupID="groupID"
-        @createTask="addCreatedTask"/>
+      </div>
+      <button
+        class="basis-16 min-h-[64px] w-full rounded-full bg-orange-300 text-white grid place-content-center"
+        @click="addTask"
+      >
+        <div class="flex flex-row gap-2 items-center">
+          <i class="bi bi-journal-plus text-2xl" />
+          <span class="text-xl">選択した課題を追加する</span>
+        </div>
+      </button>
+    </div>
+  </Modal>
+  <CreateTaskModal
+    v-model:open="showCreateTaskModal"
+    :groupID="groupID"
+    @createTask="addCreatedTask"
+  />
 </template>
 <script setup lang="ts">
-import Modal from "../modal.vue"
+import Modal from "../modal.vue";
 
 import useTasks from "../../../hooks/useTasks";
-import SharedTaskButton from "./addTaskFromSharedTaskModal/sharedTaskButton.vue"
+import SharedTaskButton from "./addTaskFromSharedTaskModal/sharedTaskButton.vue";
 import useAuth from "../../../hooks/useAuth";
 
 import CreateTaskModal from "./createTaskModal.vue";
 import { ref } from "vue";
 import { Task } from "../../../models/task";
-
+import useGroupSettings from "../../../hooks/useGroupSettings";
 
 //import useTasksAnalytics from "../../../hooks/useTasksAnalytics";
 
 const props = defineProps<{
-    open: boolean
-    groupID: string
-}>()
+  open: boolean;
+  groupID: string;
+}>();
 
 const emit = defineEmits<{
-    (e: "update:open", newOpen: boolean): void
-}>()
+  (e: "update:open", newOpen: boolean): void;
+}>();
 
-const que = ref<`${string}-${string}`[]>([])
-const addTaskToQue = (taskID: `${string}-${string}`) => {
-    if( que.value.includes(taskID) ){
-        que.value = que.value.filter((id) => id != taskID)
-    }else{
-        que.value.push(taskID)
-    }
+type TaskID = `${string}-${string}`;
+
+const addQue = ref<TaskID[]>([]);
+const addTaskToQue = (taskID: TaskID) => {
+  if (addQue.value.includes(taskID)) {
+    addQue.value = addQue.value.filter((id) => id != taskID);
+  } else {
+    addQue.value.push(taskID);
+  }
+};
+
+const { groupSettings } = await useGroupSettings(props.groupID);
+const members = groupSettings.value.members;
+
+async function promoteTask(taskID: TaskID) {
+  if (!tasks.value.find((task) => task.id == taskID)) {
+    console.log("タスクが見つかりませんでした");
+  } else {
+    Object.entries(members).forEach((data) => {
+      const [memberUid, _userData] = data;
+      tasks.value.find((task) => task.id == taskID)?.workon.push(memberUid);
+    });
+  }
 }
 
-const { tasks } = await useTasks(props.groupID)
-const { getUserData } = await useAuth()
-const { uid } = await getUserData()
+const { tasks } = await useTasks(props.groupID);
+const { getUserData } = await useAuth();
+const { uid } = await getUserData();
 
-const showCreateTaskModal = ref(false)
-function createTask(){
-    showCreateTaskModal.value = true
+const showCreateTaskModal = ref(false);
+function createTask() {
+  showCreateTaskModal.value = true;
 }
 
-function addCreatedTask(task: Task){
-    tasks.value.push(task)
-    showCreateTaskModal.value = false
+function addCreatedTask(task: Task) {
+  tasks.value.push(task);
+  showCreateTaskModal.value = false;
 }
 
 //const { logTasksAnalytics } = await useTasksAnalytics()
-function addTask(){
-    /*logTasksAnalytics({
+function addTask() {
+  /*logTasksAnalytics({
         name: "createTask",
         kadai_id: newTask.id
     })*/
-    que.value.forEach((taskID: `${string}-${string}`) => {
-        if( !tasks.value.find((task) => task.id == taskID) ){
-            console.log("タスクが見つかりませんでした")
-        }else{
-            tasks.value.find((task) => task.id == taskID)?.workon.push(uid)
-        }
-    })
-    emit("update:open", false)
+  addQue.value.forEach((taskID: `${string}-${string}`) => {
+    if (!tasks.value.find((task) => task.id == taskID)) {
+      console.log("タスクが見つかりませんでした");
+    } else {
+      tasks.value.find((task) => task.id == taskID)?.workon.push(uid);
+    }
+  });
+  emit("update:open", false);
 }
 </script>
--- src/components/App/Group/addTaskFromSharedTaskModal/sharedTaskButton.vue
+++ src/components/App/Group/addTaskFromSharedTaskModal/sharedTaskButton.vue
@@ -1,23 +1,31 @@
 <template>
-    <button
-        class="w-full p-4 bg-white/80 border-2 border-l-8 rounded-xl flex flex-col gap-2"
-        :class="{ '!bg-gray-100': isInQue }"
-        :style="{ borderColor: `${task.subject.color}70` }"
-        @click="addTaskToQue">
-        <h2 class="text-lg font-semibold">{{ task.name }}</h2>
-        <div class="flex flex-row gap-4 w-full">
-            <div class="flex flex-row h-8 basis-40">
-                <div
-                    v-for="(uid, index) in props.task.workon"
-                    :key="index"
-                    class="-ml-4 first:m-0">
-                    <UserIcon
-                        :iconURL="getMemberIcon(uid)"
-                        :color="task.subject.color"/>
+    <div
+        class="w-full p-4 bg-white/80 border-2 border-l-8 rounded-xl flex flex-row items-stretch"
+        :class="{ '!bg-gray-100': isInAddQue }"
+        :style="{ borderColor: `${task.subject.color}70` }">
+        <button
+            class="grow flex flex-col gap-2"
+            @click="addTaskToQue">
+            <h2 class="text-lg font-semibold text-left">{{ task.name }}</h2>
+            <div class="flex flex-row gap-4 w-full">
+                <div class="flex flex-row h-8 basis-40">
+                    <div
+                        v-for="(uid, index) in props.task.workon"
+                        :key="index"
+                        class="-ml-4 first:m-0">
+                        <UserIcon
+                            :iconURL="getMemberIcon(uid)"
+                            :color="task.subject.color"/>
+                    </div>
                 </div>
             </div>
-        </div>
-    </button>
+        </button>
+        <button
+            class="basis-12 grid place-content-center border-gray-200 border-2 rounded-md"
+            @click="promoteTaskToEveryone">
+            <i class="bi bi-megaphone-fill"/>
+        </button>
+    </div>
 </template>
 <script setup lang="ts">
 import { Task } from "../../../../models/task"
@@ -27,11 +35,12 @@ import useGroupSettings from "../../../../hooks/useGroupSettings";
 const props = defineProps<{
     task: Task
     groupID: string
-    isInQue: boolean
+    isInAddQue: boolean
 }>()
 
 const emit = defineEmits<{
     (e: "selected"): void
+    (e: "promoteThisTask"): void
 }>()
 
 const { groupSettings } = await useGroupSettings(props.groupID)
@@ -41,5 +50,9 @@ const getMemberIcon = (uid: string) => {
 
 const addTaskToQue = () => {
     emit("selected")
+}
+
+const promoteTaskToEveryone = () => {
+    emit("promoteThisTask")
 }
 </script>
