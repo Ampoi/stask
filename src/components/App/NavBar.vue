66565ff990aac9119a428d3d3a1cbd0f50904b70
Ampoi <tange.toshihiro@outlook.jp>
2023-08-25 13:11:58 +0900

    ナビゲーションメニューが文字入力欄と重なる問題の種入力欄と重なる問題の修正

--- src/components/App/NavBar.vue
+++ src/components/App/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="bottom-12 h-16 flex flex-row gap-4">
+    <div class="bottom-12 h-16 flex flex-row gap-4 z-30">
         <Menu as="div" class="relative">
             <MenuButton class="w-16 h-16 bg-orange-300 text-white rounded-full grid place-content-center">
                 <i class="bi bi-list text-3xl"/>
