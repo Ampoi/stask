77b0b6bc7b64daef6d1dc1aeaa060dacd6e15491
Ampoi <tange.toshihiro@outlook.jp>
2023-10-12 22:34:01 +0900

    メンバーかどうかの確認の高速化

--- src/hooks/useMember.ts
+++ src/hooks/useMember.ts
@@ -1,10 +1,19 @@
-import { memberRepository } from "../infra/memberRepository";
+//import { memberRepository } from "../infra/memberRepository";
+
+import { createGroupSettingsRepository } from "../infra/groupSettingsRepository"
 
 export default async (groupID: string) => {
     const isMember = await (async () => {
-        const { checkMember } = memberRepository
+        /*const { checkMember } = memberRepository
         const result = await checkMember({ groupID })
-        return result.data
+        return result.data*/
+        const settingsRepository = createGroupSettingsRepository(groupID)
+        try {
+            await settingsRepository.get()
+            return true
+        } catch (error) {
+            return false
+        }
     })()
 
     return { isMember }
